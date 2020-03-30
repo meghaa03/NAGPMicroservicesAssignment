30bab513d80cd1bbbcaf56f17817e01e2274b1a0
Megha Gupta <megha.gupta01@nagarro.com>
2020-03-30 23:24:23 +0530

    initial commit

---
+++ UserBDC/Classes/UserBDC.cs
@@ -0,0 +1,27 @@
+﻿using Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UserBDC.Interfaces;
+
+namespace UserBDC.Classes
+{
+    public class UserBDC : IUserBDC
+    {
+        public UserDTO userDetails;
+
+        public UserBDC()
+        {
+            userDetails = new UserDTO
+            {
+                Name = "John",
+                Age = 23,
+                Email = "john.doe@google.com"
+            };
+        }
+        public UserDTO GetUserByID(int userId)
+        {
+            return userDetails;
+        }
+    }
+}
---
+++ UserBDC/Interfaces/IUserBDC.cs
@@ -0,0 +1,12 @@
+﻿using Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserBDC.Interfaces
+{
+    public interface IUserBDC
+    {
+        UserDTO GetUserByID(int userId);
+    }
+}
